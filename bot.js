f3c14f83a6e3db7ccb1fee1e8eca718aecaa7277
Soul <soulus98@hotmail.com>
2022-07-11 13:37:23 +1000

    stopped testing

--- bot.js
+++ bot.js
@@ -108,8 +108,6 @@ client.once("ready", async () => {
 		console.log("\nOops the screenshot server is broken.");
 		return;
 	}
-	const brokenChannelMsg = client.channels.cache.get("982598490687410206").lastMessage
-	console.log(brokenChannelMsg);
 	const activeServers = client.guilds.cache;
 	const activeServerList = [];
 	activeServers.each(serv => activeServerList.push(`"${serv.name}" aka #${serv.id}`));
