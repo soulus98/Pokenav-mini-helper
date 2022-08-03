24c9b8c552733710ad343f185238f1aecc342512
Soul-Home <soulus98@hotmail.com>
2022-08-03 20:59:27 +1000

    list inline

--- commands/list.js
+++ commands/list.js
@@ -13,81 +13,80 @@ module.exports = {
 	async execute(message, args) {
 		if (args[1]) {
 			message.reply("You must specify *only one* argument.");
-			return `, but it failed, as ${args} is too many args`
+			return `, but it failed, as ${args} is too many args`;
 		}
 		switch (args[0]) {
 			case "clean":
 			case "cleanup":
-			loadCleanupList().then((list) => {
-				const chAmount = list.reduce((acc, item) => {
-					acc = acc + item.length;
-					return acc;
-				}, 0);
-				if (chAmount == 0) {
-					message.reply("There are no channels currently being filtered");
-					return;
-				}
-				const data = ["Here is a list of the currently filtered channels:"];
-				list.each((arr, group) => {
-					data.push(`**${group}:**`);
-					if (arr.length == 0) data.push("No channels");
-					else arr.forEach((channel) => {
-						data.push(`<#${channel}>`);
+				loadCleanupList().then((list) => {
+					const chAmount = list.reduce((acc, item) => {
+						acc = acc + item.length;
+						return acc;
+					}, 0);
+					if (chAmount == 0) {
+						message.reply("There are no channels currently being filtered");
+						return;
+					}
+					const data = ["Here is a list of the currently filtered channels:"];
+					list.each((arr, group) => {
+						data.push(`**${group}:**`);
+						if (arr.length == 0) data.push("No channels");
+						else arr.forEach((channel) => {
+							data.push(`<#${channel}>`);
+						});
+						data.push("");
 					});
-					data.push("");
+					message.reply(data.join("\n"));
+					return;
 				});
-				message.reply(data.join("\n"));
-				return;
-			});
 				return;
 			case "categories":
 			case "cats":
 			case "catswitch":
-			loadRaidCatList().then((list) => {
-				const catAmount = list.reduce((acc, item) => {
-					acc = acc + item.length;
-					return acc;
-				}, 0);
-				if (catAmount == 0) {
-					message.reply("There are no raid categories currently being watched");
-					return;
-				}
-				const data = ["Here is a list of the currently watched raid categories and their linked announce channels:"];
-				list.each((arr, group) => {
-					data.push(`<#${group}>:`);
-					if (arr.length == 0) data.push("No channels");
-					else arr.forEach((channel) => {
-						data.push(`<#${channel}>`);
+				loadRaidCatList().then((list) => {
+					const catAmount = list.reduce((acc, item) => {
+						acc = acc + item.length;
+						return acc;
+					}, 0);
+					if (catAmount == 0) {
+						message.reply("There are no raid categories currently being watched");
+						return;
+					}
+					const data = ["Here is a list of the currently watched raid categories and their linked announce channels:"];
+					list.each((arr, group) => {
+						data.push(`<#${group}>:`);
+						if (arr.length == 0) data.push("No channels");
+						else arr.forEach((channel) => {
+							data.push(`<#${channel}>`);
+						});
+						data.push("");
 					});
-					data.push("");
+					message.reply(data.join("\n"));
+					return;
 				});
-				message.reply(data.join("\n"));
-				return;
-			});
 				return;
 			case "notifications":
 			case "notis":
-			loadNotifyList().then((list) => {
-				const bossAmount = list.reduce((acc, g) => {
-					acc = acc + g.length;
-					return acc;
-				}, 0);
-				if (bossAmount == 0) {
-					message.reply("There are no boss notifications at the moment");
-					return;
-				}
-				const data = ["Here is a list of the current notifications:"];
-				list.each((arr, group) => {
-					data.push(`**${group}:**`);
-					if (arr.length == 0) data.push("No bosses");
-					else arr.forEach((item) => {
-						data.push(`<:${item.identifier}>`);
+				loadNotifyList().then((list) => {
+					const bossAmount = list.reduce((acc, g) => {
+						acc = acc + g.length;
+						return acc;
+					}, 0);
+					if (bossAmount == 0) {
+						message.reply("There are no boss notifications at the moment");
+						return;
+					}
+					const data = ["Here is a list of the current notifications:"];
+					list.each((arr, group) => {
+						data.push(`\n\n**${group}:**\n`);
+						if (arr.length == 0) data.push("No bosses");
+						else arr.forEach((item) => {
+							data.push(`<:${item.identifier}>`);
+						});
 					});
-					data.push("");
+					message.reply(data.join(""));
+					return;
 				});
-				message.reply(data.join("\n"));
-				return;
-			});
 				return;
 			default:
 				message.reply("Type must be either `cleanup`, `categories`, or `notifications`");
